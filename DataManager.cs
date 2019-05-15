457622775c06f8c77a8dc7f3fbc6a46a2d5ee7c7
dlrjones <robjon@keymoon.net>
2019-05-15 10:03:42 -0700

    Add files via upload

--- DataManager.cs
+++ DataManager.cs
@@ -95,10 +95,38 @@ namespace WaspRefresh
 
         private string BuildQuery()
         {
-            string query = "SELECT TOP(100) PERCENT dbo.ITEM.ITEM_ID, dbo.ITEM.ITEM_NO, RTRIM(dbo.REQ.REQ_NO) AS REQ_NO, dbo.REQ_ITEM.LINE_NO, " +
+            string query = "DECLARE @EndDate DATETIME = (SELECT GETDATE()) " +
+                            "DECLARE @AVG_DAYS float " +
+                            "CREATE TABLE #PO_ACTIVITY (" +
+                            "ITEM_NO varchar (20), " +
+                            "AVG_DAYS_TO_DELV float, " +
+                            "PO_TYPE INT )" +
+                            "INSERT INTO #PO_ACTIVITY " +
+                            "SELECT I.ITEM_NO, " +
+                            "AVG(CONVERT(float, DATEDIFF(day, PO.PO_DATE, PL.LAST_RCV_DATE))) AVG_DAYS_TO_DELIVERY,PO_TYPE " +
+                            "FROM PO " +
+                            "INNER JOIN PO_LINE PL ON PO.PO_ID = PL.PO_ID " +
+                            "INNER JOIN ITEM I ON PL.ITEM_ID = I.ITEM_ID " +
+                            "INNER JOIN PO_SUB_LINE PSL ON PL.PO_LINE_ID = PSL.PO_LINE_ID " +
+                            "INNER JOIN CC ON PSL.CC_ID = CC.CC_ID " +
+                            "WHERE PL.STAT IN(3,10) " +
+                            "AND PO.PO_DATE BETWEEN DATEADD(day, -365, @EndDate) AND DATEADD(ms,-3,DATEADD(day, 1, @EndDate)) " +
+                            "GROUP BY I.ITEM_NO,PO_TYPE " +
+
+                "SELECT TOP(100) PERCENT dbo.ITEM.ITEM_ID, dbo.ITEM.ITEM_NO, RTRIM(dbo.REQ.REQ_NO) AS REQ_NO, dbo.REQ_ITEM.LINE_NO, " +
                     "dbo.ITEM.DESCR, dbo.REQ_ITEM.QTY AS PAR, SUBSTRING(dbo.REQ_ITEM.UM_CD, 7, 2) AS[PAR UM], dbo.ITEM.CTLG_NO, " +
                   "RIGHT(RTRIM(dbo.ITEM_VEND_PKG.UM_CD), 2) + ' ' + CAST(RIGHT(RTRIM(dbo.ITEM_VEND_PKG_FACTOR.TO_QTY), 4) AS VARCHAR) " +
-                  " + ' ' + RIGHT(RTRIM(dbo.ITEM_VEND_PKG.TO_UM_CD), 2) AS PKG_STR, dbo.REQ_ITEM.PAR_BIN_LOC AS[BIN LOC], dbo.VEND.LEAD_DAYS " +
+                  " + ' ' + RIGHT(RTRIM(dbo.ITEM_VEND_PKG.TO_UM_CD), 2) AS PKG_STR, " +
+                  "CASE " +
+                    "WHEN LOC.LOC_TYPE = 1 THEN REQ_ITEM.PAR_BIN_LOC " +
+                    "ELSE SLOC_ITEM_BIN.BIN_LOC " +
+                    "END AS[BIN LOC],  " +
+                    "CASE " +
+                    "WHEN POA.AVG_DAYS_TO_DELV < 1.0 THEN 1.00 " +
+                    "WHEN POA.PO_TYPE = 2 THEN 1.00 " +
+                    "ELSE CAST(POA.AVG_DAYS_TO_DELV AS decimal(9,2)) " +
+                    "END AS LEAD_DAYS " +
+
                   "FROM dbo.REQ INNER JOIN dbo.REQ_ITEM ON dbo.REQ.REQ_ID = dbo.REQ_ITEM.REQ_ID INNER JOIN " +
                 "dbo.ITEM ON dbo.REQ_ITEM.ITEM_ID = dbo.ITEM.ITEM_ID LEFT OUTER JOIN " +
                 "dbo.ITEM_VEND ON dbo.ITEM.ITEM_ID = dbo.ITEM_VEND.ITEM_ID INNER JOIN " +
@@ -106,14 +134,21 @@ namespace WaspRefresh
                 "dbo.ITEM_VEND_PKG_FACTOR ON dbo.ITEM_VEND_PKG.ITEM_VEND_ID = dbo.ITEM_VEND_PKG_FACTOR.ITEM_VEND_ID AND " +
                 "dbo.ITEM_VEND_PKG.UM_CD = dbo.ITEM_VEND_PKG_FACTOR.UM_CD AND " +
                 "dbo.ITEM_VEND_PKG.TO_UM_CD = dbo.ITEM_VEND_PKG_FACTOR.TO_UM_CD LEFT OUTER JOIN " +
-                "dbo.VEND ON dbo.VEND.VEND_ID = dbo.ITEM_VEND.VEND_ID " +
-                "WHERE(dbo.ITEM.STAT = 1) AND(dbo.ITEM_VEND.SEQ_NO = 1) AND(dbo.ITEM_VEND_PKG.SEQ_NO = 1) AND(dbo.REQ.REQ_TYPE = 3) AND " +
-                "(dbo.REQ.STAT = 13) AND(dbo.REQ_ITEM.QTY > 0) AND(dbo.ITEM_VEND.CORP_ID = 1000)  " +
-                "GROUP BY dbo.ITEM.ITEM_ID, dbo.ITEM.ITEM_NO, dbo.REQ.REQ_NO, dbo.REQ_ITEM.LINE_NO, dbo.ITEM.DESCR,  " +
-                "dbo.REQ_ITEM.QTY, dbo.REQ_ITEM.UM_CD, dbo.ITEM.CTLG_NO, dbo.ITEM_VEND_PKG.UM_CD, " +
-                "dbo.ITEM_VEND_PKG_FACTOR.TO_QTY, dbo.ITEM_VEND_PKG.TO_UM_CD, dbo.REQ_ITEM.PAR_BIN_LOC, " +
-                "dbo.VEND.LEAD_DAYS " +
-                "ORDER BY REQ_NO, dbo.REQ_ITEM.LINE_NO";
+                "#PO_ACTIVITY POA on POA.ITEM_NO = ITEM.ITEM_NO JOIN " +
+                "SLOC_ITEM_BIN ON SLOC_ITEM_BIN.ITEM_ID = REQ_ITEM.ITEM_ID JOIN " +
+                "LOC ON LOC.LOC_ID = SLOC_ITEM_BIN.LOC_ID " +
+
+                "WHERE(ITEM.STAT IN(1, 2)) AND(ITEM_VEND.SEQ_NO = 1) AND(ITEM_VEND_PKG.SEQ_NO = 1) AND(REQ.REQ_TYPE = 3) AND " +
+                "(REQ.STAT = 13) AND(REQ_ITEM.QTY > 0) AND(ITEM_VEND.CORP_ID = 1000) " +
+
+                "GROUP BY ITEM.ITEM_ID, ITEM.ITEM_NO, REQ.REQ_NO, REQ_ITEM.LINE_NO, ITEM.DESCR, " +
+                "REQ_ITEM.QTY, REQ_ITEM.UM_CD, ITEM.CTLG_NO, ITEM_VEND_PKG.UM_CD,  " +
+                "ITEM_VEND_PKG_FACTOR.TO_QTY, ITEM_VEND_PKG.TO_UM_CD, REQ_ITEM.PAR_BIN_LOC,  " +
+                "POA.AVG_DAYS_TO_DELV, POA.PO_TYPE " +
+                ",LOC.LOC_TYPE,SLOC_ITEM_BIN.BIN_LOC " +
+
+                "ORDER BY REQ_NO, REQ_ITEM.LINE_NO " +
+                "DROP TABLE #PO_ACTIVITY";
             return query;
         }
        
--- ReadMe.txt
+++ ReadMe.txt
@@ -26,3 +26,79 @@ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMA
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			*************************************************
+			query for items using par form bin locations (REQ_ITEM.PAR_BIN_LOC table)
+			*************************************************
+
+ string query = "SELECT TOP(100) PERCENT dbo.ITEM.ITEM_ID, dbo.ITEM.ITEM_NO, RTRIM(dbo.REQ.REQ_NO) AS REQ_NO, dbo.REQ_ITEM.LINE_NO, " +
+                    "dbo.ITEM.DESCR, dbo.REQ_ITEM.QTY AS PAR, SUBSTRING(dbo.REQ_ITEM.UM_CD, 7, 2) AS[PAR UM], dbo.ITEM.CTLG_NO, " +
+                  "RIGHT(RTRIM(dbo.ITEM_VEND_PKG.UM_CD), 2) + ' ' + CAST(RIGHT(RTRIM(dbo.ITEM_VEND_PKG_FACTOR.TO_QTY), 4) AS VARCHAR) " +
+                  " + ' ' + RIGHT(RTRIM(dbo.ITEM_VEND_PKG.TO_UM_CD), 2) AS PKG_STR, dbo.REQ_ITEM.PAR_BIN_LOC AS[BIN LOC], dbo.VEND.LEAD_DAYS " +
+
+                  "FROM dbo.REQ INNER JOIN dbo.REQ_ITEM ON dbo.REQ.REQ_ID = dbo.REQ_ITEM.REQ_ID INNER JOIN " +
+                "dbo.ITEM ON dbo.REQ_ITEM.ITEM_ID = dbo.ITEM.ITEM_ID LEFT OUTER JOIN " +
+                "dbo.ITEM_VEND ON dbo.ITEM.ITEM_ID = dbo.ITEM_VEND.ITEM_ID INNER JOIN " +
+                "dbo.ITEM_VEND_PKG ON dbo.ITEM_VEND.ITEM_VEND_ID = dbo.ITEM_VEND_PKG.ITEM_VEND_ID INNER JOIN " +
+                "dbo.ITEM_VEND_PKG_FACTOR ON dbo.ITEM_VEND_PKG.ITEM_VEND_ID = dbo.ITEM_VEND_PKG_FACTOR.ITEM_VEND_ID AND " +
+                "dbo.ITEM_VEND_PKG.UM_CD = dbo.ITEM_VEND_PKG_FACTOR.UM_CD AND " +
+                "dbo.ITEM_VEND_PKG.TO_UM_CD = dbo.ITEM_VEND_PKG_FACTOR.TO_UM_CD LEFT OUTER JOIN " +
+                "dbo.VEND ON dbo.VEND.VEND_ID = dbo.ITEM_VEND.VEND_ID " +
+
+                "WHERE(dbo.ITEM.STAT = 1) AND(dbo.ITEM_VEND.SEQ_NO = 1) AND(dbo.ITEM_VEND_PKG.SEQ_NO = 1) AND(dbo.REQ.REQ_TYPE = 3) AND " +
+                "(dbo.REQ.STAT = 13) AND(dbo.REQ_ITEM.QTY > 0) AND(dbo.ITEM_VEND.CORP_ID = 1000)  " +
+
+                "GROUP BY dbo.ITEM.ITEM_ID, dbo.ITEM.ITEM_NO, dbo.REQ.REQ_NO, dbo.REQ_ITEM.LINE_NO, dbo.ITEM.DESCR,  " +
+                "dbo.REQ_ITEM.QTY, dbo.REQ_ITEM.UM_CD, dbo.ITEM.CTLG_NO, dbo.ITEM_VEND_PKG.UM_CD, " +
+                "dbo.ITEM_VEND_PKG_FACTOR.TO_QTY, dbo.ITEM_VEND_PKG.TO_UM_CD, dbo.REQ_ITEM.PAR_BIN_LOC, " +
+                "dbo.VEND.LEAD_DAYS " +
+
+                "ORDER BY REQ_NO, dbo.REQ_ITEM.LINE_NO";
+            return query;
+
+
+			*************************************************
+			query for items in med stores (SLOC_ITEM_BIN table)
+			*************************************************
+
+SELECT TOP (100) PERCENT ITEM.ITEM_ID, ITEM.ITEM_NO, RTRIM(REQ.REQ_NO) AS REQ_NO, REQ_ITEM.LINE_NO, 
+                  ITEM.DESCR, REQ_ITEM.QTY AS PAR, SUBSTRING(REQ_ITEM.UM_CD, 7, 2) AS [PAR UM], ITEM.CTLG_NO, 
+                  RIGHT(RTRIM(ITEM_VEND_PKG.UM_CD), 2) + ' ' + CAST(RIGHT(RTRIM(ITEM_VEND_PKG_FACTOR.TO_QTY), 4) AS VARCHAR) 
+                  + ' ' + RIGHT(RTRIM(ITEM_VEND_PKG.TO_UM_CD), 2) AS PKG_STR,
+				SLOC_ITEM_BIN.BIN_LOC AS [BIN LOC], 
+                  VEND.LEAD_DAYS
+				
+FROM     REQ INNER JOIN
+                  REQ_ITEM ON REQ.REQ_ID = REQ_ITEM.REQ_ID INNER JOIN				  
+                  ITEM ON REQ_ITEM.ITEM_ID = ITEM.ITEM_ID LEFT OUTER JOIN
+                  ITEM_VEND ON ITEM.ITEM_ID = ITEM_VEND.ITEM_ID INNER JOIN
+                  ITEM_VEND_PKG ON ITEM_VEND.ITEM_VEND_ID = ITEM_VEND_PKG.ITEM_VEND_ID INNER JOIN
+                  ITEM_VEND_PKG_FACTOR ON ITEM_VEND_PKG.ITEM_VEND_ID = ITEM_VEND_PKG_FACTOR.ITEM_VEND_ID AND 
+                  ITEM_VEND_PKG.UM_CD = ITEM_VEND_PKG_FACTOR.UM_CD AND 
+                  ITEM_VEND_PKG.TO_UM_CD = ITEM_VEND_PKG_FACTOR.TO_UM_CD INNER JOIN				  
+                  VEND ON VEND.VEND_ID = ITEM_VEND.VEND_ID LEFT OUTER JOIN
+				  SLOC_ITEM_BIN ON SLOC_ITEM_BIN.ITEM_ID = REQ_ITEM.ITEM_ID 
+				   LEFT OUTER JOIN SLOC_ITEM ON SLOC_ITEM.ITEM_ID = SLOC_ITEM_BIN.ITEM_ID
+
+WHERE  (SLOC_ITEM.STAT IN (1,2)) AND (ITEM_VEND.SEQ_NO = 1) AND (ITEM_VEND_PKG.SEQ_NO = 1) AND (REQ.REQ_TYPE = 3) AND 
+                  (REQ.STAT = 13) AND (REQ_ITEM.QTY > 0) AND (ITEM_VEND.CORP_ID = 1000)
+GROUP BY ITEM.ITEM_ID, ITEM.ITEM_NO, REQ.REQ_NO, REQ_ITEM.LINE_NO, ITEM.DESCR, 
+				  SLOC_ITEM_BIN.BIN_LOC,
+                  REQ_ITEM.QTY, REQ_ITEM.UM_CD, ITEM.CTLG_NO, ITEM_VEND_PKG.UM_CD, 
+				  REQ_ITEM.REQ_ITEM_ID,
+                  ITEM_VEND_PKG_FACTOR.TO_QTY, ITEM_VEND_PKG.TO_UM_CD, REQ_ITEM.PAR_BIN_LOC, 
+                  VEND.LEAD_DAYS
+ORDER BY REQ_NO, REQ_ITEM.LINE_NO 
